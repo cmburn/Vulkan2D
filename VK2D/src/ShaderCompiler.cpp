3bf9a49cb373974e5fed8a78fd05a82a2dfcec1d
Paolo Mazzon <pdmazzon@gmail.com>
2026-01-13 18:41:50 -0800

    Shader compile validates (almost) all parts of the user's shader to make sure it wont cause a weird crash

--- VK2D/src/ShaderCompiler.cpp
+++ VK2D/src/ShaderCompiler.cpp
@@ -71,20 +71,98 @@ bool _vk2dShaderCompile(const char *shader, uint32_t shaderSize, VK2DCompiledSha
         return false;
     }
 
-    // Detect if the user has a buffer in the shader
+    // Detect a user buffer automatically and make sure the inputs are structured properly
     auto programLayout = program->getLayout();
     auto globalVarLayout = programLayout->getGlobalParamsVarLayout();
     auto globalTypeLayout = globalVarLayout->getTypeLayout();
     size_t userDataSize = 0;
+    bool matchingPushConstants = false;
+    bool matchingSampler = false;
+    bool matchingTextures = false;
+    bool extraGarbage = false;
 
     int fieldCount = globalTypeLayout->getFieldCount();
     for (int i = 0; i < fieldCount; i++) {
+        // Get information about this field
         auto fieldVarLayout = globalTypeLayout->getFieldByIndex(i);
-        if (strcmp(fieldVarLayout->getName(), "userData") == 0) {
-            auto fieldTypeLayout = fieldVarLayout->getTypeLayout();
-            userDataSize = fieldTypeLayout->getSize();
-            break;
+        auto fieldTypeLayout = fieldVarLayout->getTypeLayout();
+        int32_t descriptorBinding = fieldVarLayout->getBindingIndex();
+        int32_t descriptorSet = fieldVarLayout->getBindingSpace();
+        int32_t size = userDataSize = fieldTypeLayout->getElementTypeLayout()->getSize();
+        Slang::ComPtr<IBlob> tempBlob;
+        fieldTypeLayout->getType()->getFullName(tempBlob.writeRef());
+        const char *typeFullName = static_cast<const char *>(tempBlob->getBufferPointer());
+
+        // Check for extra user-provided data to the shader
+        if (descriptorBinding == 3 && descriptorSet == 3) {
+            if (strcmp("ConstantBuffer", fieldTypeLayout->getName()) != 0) {
+                vk2dLogInfo("User-provided data to shaders must be a ConstantBuffer, not \"%s\".", fieldTypeLayout->getName());
+                return false;
+            }
+            userDataSize = size;
+            continue;
         }
+
+        // Validate push constants range
+        if (strcmp(fieldVarLayout->getName(), "push") == 0) {
+            if (strcmp("ConstantBuffer<Push, DefaultPushConstantDataLayout>", typeFullName) != 0) {
+                vk2dLogInfo("Push constant is of incorrect type \"%s\", it should be \"[[vk::push_constant]] Push push;\"", typeFullName);
+                return false;
+            }
+            if (size != 112) {
+                vk2dLogInfo("Push constant range is of incorrect size \"%i\", it should be 112 bytes. Please check https://paolomazzon.github.io/Vulkan2D/md_docs_2Shaders.html for more information.", size);
+                return false;
+            }
+            matchingPushConstants = true;
+            continue;
+        }
+
+        // Validate matching sampler input
+        if (strcmp(fieldVarLayout->getName(), "sampler") == 0) {
+            // "SamplerState" 1/1
+            if (strcmp("SamplerState", typeFullName) != 0) {
+                vk2dLogInfo("Sampler is of incorrect type \"%s\", it should be \"[[vk::binding(1,1)]] SamplerState sampler;\"", typeFullName);
+                return false;
+            }
+            if (descriptorSet != 1 || descriptorBinding != 1) {
+                vk2dLogInfo("Sampler is in the incorrect binding/set [%i/%i], it should be \"[[vk::binding(1,1)]] SamplerState sampler;\"", descriptorBinding, descriptorSet);
+                return false;
+            }
+
+            matchingSampler = true;
+            continue;
+        }
+
+        // Validate matching texture input
+        if (strcmp(fieldVarLayout->getName(), "textures") == 0) {
+            if (strcmp("Texture2D<vector<float,4>>[]", typeFullName) != 0) {
+                vk2dLogInfo("Texture array is of incorrect type \"%s\", it should be \"[[vk::binding(2,2)]] Texture2D<float4> textures[];\"", typeFullName);
+                return false;
+            }
+            if (descriptorSet != 2 || descriptorBinding != 2) {
+                vk2dLogInfo("Texture array is in the incorrect binding/set [%i/%i]. It should be \"[[vk::binding(2,2)]] Texture2D<float4> textures[];\"", descriptorBinding, descriptorSet);
+                return false;
+            }
+
+            matchingTextures = true;
+            continue;
+        }
+
+        // Getting here means this field is invalid
+        vk2dLogInfo("Invalid field \"%s\" of type \"%s\" found in shader. Please check https://paolomazzon.github.io/Vulkan2D/md_docs_2Shaders.html for more information.", fieldVarLayout->getName(), typeFullName);
+        return false;
+    }
+
+    // Make sure everything was found
+    if (!matchingTextures || !matchingSampler || !matchingPushConstants) {
+        if (!matchingSampler)
+            vk2dLogInfo("Sampler state missing in shader. Please check https://paolomazzon.github.io/Vulkan2D/md_docs_2Shaders.html for more information.");
+        if (!matchingTextures)
+            vk2dLogInfo("Texture array missing in shader. Please check https://paolomazzon.github.io/Vulkan2D/md_docs_2Shaders.html for more information.");
+        if (!matchingPushConstants)
+            vk2dLogInfo("Push constants missing in shader. Please check https://paolomazzon.github.io/Vulkan2D/md_docs_2Shaders.html for more information.");
+
+        return false;
     }
 
     // Link the shader
